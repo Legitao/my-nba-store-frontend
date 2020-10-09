f4f8366f5a7597001931b26ac08ea8cbfcb3839b
Tao Zhu <zhutaosx@gmail.com>
2020-10-09 00:47:04 -0700

    change some text to test docker hub and azure CD

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>ShopName</Navbar.Brand>
+            <Navbar.Brand>TZ's NBA Store</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
