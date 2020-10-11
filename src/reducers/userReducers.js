a93772b8fec5125ef8001a24da4104936225be83
Tao Zhu <zhutaosx@gmail.com>
2020-10-11 13:09:56 -0700

    Add profile screen

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -6,6 +6,12 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -30,6 +36,32 @@ export const userRegisterReducer = (state = {}, action) => {
     case USER_REGISTER_SUCCESS:
       return { pending: false, userInfo: action.payload };
     case USER_REGISTER_FAIL:
+      return { pending: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { pending: true };
+    case USER_DETAILS_SUCCESS:
+      return { pending: false, userProfile: action.payload };
+    case USER_DETAILS_FAIL:
+      return { pending: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { pending: true };
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { pending: false, success: true, userProfile: action.payload };
+    case USER_UPDATE_PROFILE_FAIL:
       return { pending: false, error: action.payload };
     default:
       return state;
